c357c47f1d7db17990a47c0e7df27eced820fed9
JGodwyn <60453471+JGodwyn@users.noreply.github.com>
2024-12-28 10:54:45 +0100

    Initial commit to get started

---
+++ SwiftAuth/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftAuth
+//
+//  Created by Gdwn on 28/12/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ SwiftAuth/SwiftAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAuthApp.swift
+//  SwiftAuth
+//
+//  Created by Gdwn on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
